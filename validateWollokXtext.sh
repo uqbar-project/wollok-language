29938442871a7cc8e9b84be6cf00ac0b4a9326ee
Fernando Dodino <fernando.dodino@gmail.com>
2019-11-15 09:44:44 -0300

    enhanced script => adding configurable WOLLOK_DEV_ROOT

--- validateWollokXtext.sh
+++ validateWollokXtext.sh
@@ -1,8 +1,20 @@
-# Validate Wollok Xtext
+# Script
+# - builds Wollok core libraries
+# - and runs sanity tests
+# in Wollok Xtext environment
 #
-# 1.0
+# 1.1
 #
-# You need to run this in the root folder
+# You need to run this in the root folder of wollok-language
+# which should have wollok (xtext implementation) as parent folder
 # 
-# TODO: detect if WOLLOK_DEV_ROOT is already present
-rm -rf .history/ && cd src && WOLLOK_DEV_ROOT=../.. wollok build && cd .. && WOLLOK_DEV_ROOT=../ wollok test -dontValidate
+DEV_MODE=$1
+
+if [ -z "$WOLLOK_DEV_ROOT" ];
+then
+  BASE_WOLLOK_DEV_ROOT=../
+else
+  BASE_WOLLOK_DEV_ROOT=WOLLOK_DEV_ROOT
+fi
+
+rm -rf .history/ && cd src && WOLLOK_DEV_ROOT=$BASE_WOLLOK_DEV_ROOT/.. wollok build && cd .. && WOLLOK_DEV_ROOT=$BASE_WOLLOK_DEV_ROOT wollok test -dontValidate
